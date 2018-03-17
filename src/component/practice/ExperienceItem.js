26cf54ee440ebb0876bede543b20f5a8a5959f5d
houser <houserqu@qq.com>
2018-03-17 17:38:53 +0800

    experience

--- src/component/practice/ExperienceItem.js
+++ src/component/practice/ExperienceItem.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const Experienceitem = ({data}) => {
+  const {title, start_time, end_time, responsibility, logo} = data;
   return (
     <div className='experience__item'>
-      {data.title}
+      <div className="logo">
+        <img src={logo} alt={title}/>
+      </div>
+      <div className="experienceitem__main">
+        <div className="title">
+          {title}  <span className="time">{start_time} - {end_time}</span>
+        </div>
+        <div className="responsibility">
+          {responsibility}
+        </div>
+      </div>
     </div>
   );
 }
