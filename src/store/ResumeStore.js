8c6faa208577ad51bf3ee29b6bbbf9911aa1e723
houser <houserqu@qq.com>
2018-04-27 13:58:55 +0800

    create github page

--- src/store/ResumeStore.js
+++
@@ -1,9 +0,0 @@
-import { observable } from "mobx";
-import data_practice from '../data/practice.json';
-
-export class ResumeStore {
-  @observable data = data_practice;
-}
-
-export default new ResumeStore();
-
