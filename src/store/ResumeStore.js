ec7e1d74e81024c7fa7b1398cda2e7922b2fd455
houser <houserqu@qq.com>
2018-07-29 13:53:30 +0800

    feat:1.修改成校招简历2.改背景色

--- src/store/ResumeStore.js
+++ src/store/ResumeStore.js
@@ -2,7 +2,7 @@ import { observable } from "mobx";
 import data_practice from '../data/practice.json';
 
 export class ResumeStore {
-  @observable data = data_practice;
+  @observable data =  data_practice;
 }
 
 export default new ResumeStore();
